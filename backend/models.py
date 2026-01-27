8d0c594e9d324a7a2e0f139a47290bbeaec4e2a5
RoyCheruiyot <roycheruiyot168@gmail.com>
2026-01-27 22:19:50 +0300

    created vehicle model

--- backend/models.py
+++ backend/models.py
@@ -15,6 +15,7 @@ class User(db.Model):
     role_id = db.Column(db.Integer, db.ForeignKey('user_roles.id'), nullable=False)
 
     role = db.relationship('UserRole', back_populates='users')
+    vehicles = db.relationship('Vehicle', back_populates='user')
     
 class UserRole(db.Model):
     __tablename__ = 'user_roles'
@@ -22,6 +23,21 @@ class UserRole(db.Model):
     name = db.Column(db.String(50), unique=True, nullable=False)
 
     users = db.relationship('User', back_populates='role')
+    
+class Vehicle(db.Model):
+    __tablename__ = 'vehicles'
+    id = db.Column(db.Integer, primary_key=True)
+    route_id = db.Column(db.Integer, db.ForeignKey('routes.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    license_plate = db.Column(db.String(20), unique=True, nullable=False)
+    model = db.Column(db.String(100), nullable=False)
+    capacity = db.Column(db.Integer, nullable=False)
+
+    bookings = db.relationship('Booking', back_populates='vehicle')
+    user = db.relationship('User', back_populates='vehicles')
+    route = db.relationship('Route', back_populates='vehicles')
+    
+
     
     
 
