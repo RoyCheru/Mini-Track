016f6979fc46eca64613e89c18f43054304904f6
RoyCheruiyot <roycheruiyot168@gmail.com>
2026-01-27 22:13:48 +0300

    created the models files

---
+++ backend/models.py
@@ -0,0 +1,22 @@
+# users
+# vehicles
+# bookings
+# user_roles
+# routes
+# parents
+# school_locations
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    role_id = db.Column(db.Integer, db.ForeignKey('user_roles.id'), nullable=False)
+
+    role = db.relationship('UserRole', back_populates='users')
+    bookings = db.relationship('Booking', back_populates='user')
+    
+    
+
+
