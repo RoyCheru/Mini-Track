ccc2825adb42052e1cdc407b3bf8ad7de5c78bc5
heeba <heebahassan182@gmail.com>
2026-01-29 08:02:26 +0300

    feat: add email input field

--- frontend/client/app/authentication/login.tsx
+++ frontend/client/app/authentication/login.tsx
@@ -2,12 +2,25 @@
 
 export default function SignInPage() {
   return (
-   <div className="min-h-screen flex items-center justify-center">
+<div className="min-h-screen flex items-center justify-center">
   <div className="w-full max-w-md bg-white p-8 rounded-xl shadow-lg">
-<h1 className="text-2xl font-bold text-center mb-6">
-  Sign in to your account
-</h1>
+   <h1 className="text-2xl font-bold text-center mb-6">
+    Sign in to your account
+   </h1>
   </div>
+
+  <div>
+  <label className="block text-sm font-medium mb-1">
+    Email address
+  </label>
+  <input
+    type="email"
+    placeholder="you@example.com"
+    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+  />
+</div>
+
+
 </div>
 
   );
