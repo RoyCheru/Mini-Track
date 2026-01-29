3883f8473047da8b3656448f654014229d17db97
heeba <heebahassan182@gmail.com>
2026-01-29 08:03:18 +0300

    feat: add password input field

--- frontend/client/app/authentication/login.tsx
+++ frontend/client/app/authentication/login.tsx
@@ -19,6 +19,16 @@ export default function SignInPage() {
     className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
   />
 </div>
+ <div>
+  <label className="block text-sm font-medium mb-1">
+    Password
+  </label>
+  <input
+    type="password"
+    placeholder="••••••••"
+    className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-black"
+  />
+</div>
 
 
 </div>
