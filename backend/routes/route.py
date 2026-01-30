6f6252fd2d65f0f6436989d0feebc773d49a50a8
Mr-Dan-kibet <kibetdan56@gmail.com>
2026-01-30 16:21:19 +0300

    defines a helper function to return responses in JSON

--- backend/routes/route.py
+++ backend/routes/route.py
@@ -1,3 +1,13 @@
 from flask import request
 from flask_restful import Resource
 from models import db, Route
+
+# 1. HELPER FUNCTIONS 
+
+def serialize_route(route):
+    return {
+        "id": route.id,
+        "name": route.name,
+        "starting_point": route.starting_point,
+        "ending_point": route.ending_point
+    }
