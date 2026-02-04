4d61b66eb669d3b636793901d8282023e391298b
heeba <heebahassan182@gmail.com>
2026-02-04 17:17:23 +0300

    chore: define earnings props interface

--- frontend/client/app/dashboard/drivers/components/Earnings.tsx
+++ frontend/client/app/dashboard/drivers/components/Earnings.tsx
@@ -1,3 +1,17 @@
 export default function Earnings() {
+  
+  interface EarningsProps {
+  earnings: {
+    today: number
+    thisWeek: number
+    thisMonth: number
+    totalTrips: number
+    avgRating: number
+  }
+}
+
+export default function Earnings({ earnings }: EarningsProps) {
   return null
+}
+  
 }
